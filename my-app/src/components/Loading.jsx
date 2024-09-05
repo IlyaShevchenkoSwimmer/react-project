45902fdfb135cbb58e534e81fa2e12d63cf7e404
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2024-09-05 18:22:41 +0000

    loading css half anim

--- my-app/src/components/Loading.jsx
+++ my-app/src/components/Loading.jsx
@@ -1,9 +1,11 @@
+import './Loading.css'
+
 function Loading({text}) {
     let loadingText =[]
     for (let i = 0; i < text.length; i++) {
-        loadingText.push(<div key={i}>{text[i]}</div>)
+        loadingText.push(<div style={{animation: "loading-anim 2s ease-in-out infinite"}} className="loading-letter" key={i}>{text[i]}</div>)
     }
-    return loadingText
+    return <div className="loading">{loadingText}</div>
 }
 
 export default Loading
